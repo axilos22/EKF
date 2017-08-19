e3c252d4a14dfe0b021663ac603d5043362f0694
axilos22 <zenos_22@hotmail.com>
2017-08-20 00:50:33 +0200

    FIX: covariance matrix use matrix as object -Need to decide if using object or pointers into internal referencing. Priority given to execution speed for large matrices.

--- src/covariancematrix.cpp
+++ src/covariancematrix.cpp
@@ -1,6 +1,15 @@
 #include "covariancematrix.h"
 
-CovarianceMatrix::CovarianceMatrix(int size)
+CovarianceMatrix::CovarianceMatrix(int size) :
+    m_size(size)
 {
+    m_matrix = Eigen::MatrixXd(m_size,m_size);
+}
 
+void CovarianceMatrix::setDiagonal(double *diagValues)
+{
+    for(int i=0;i<m_size;i++)
+    {
+        m_matrix(i,i) = diagValues[i];
+    }
 }
--- src/covariancematrix.h
+++ src/covariancematrix.h
@@ -1,15 +1,20 @@
 #ifndef COVARIANCEMATRIX_H
 #define COVARIANCEMATRIX_H
 
-#include <Eigen\Dense>
+#ifdef __linux__
+    #include <eigen3/Eigen/Dense>
+#elif _WIN32
+    #include <Eigen/Dense>
+#endif
 
 class CovarianceMatrix
 {
 public:
     CovarianceMatrix(int size);
+    void setDiagonal(double* diagValues);
 private:
-    unsigned int m_size;
-	Eigen::MatrixXd* m_matrix;
+    int m_size;
+    Eigen::MatrixXd m_matrix;
 };
 
 #endif // COVARIANCEMATRIX_H
