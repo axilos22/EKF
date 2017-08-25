72723ffa326061e5c1d31141c39fbf7728470fe5
axilos22 <zenos_22@hotmail.com>
2017-08-25 19:51:30 +0200

    ADD: test code checking rank getting and setting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include "core/state.h"
+#include "core/observationmatrix.h"
+
+/*TODO :
+ * Separate all elements as sub folder: filter / filterTool / filterImplementation (src)
+*/
 
 using namespace std;
-#include "filter.h"
+using namespace Eigen;
+
+
+void stateTest() {
+    State x(2);
+    double vals[2] = {2,3};
+    x.init(vals);
+    VectorXd state = x.getVector();
+    cout << state << endl;
+}
+
+void obsMatrixTest() {
+    ObservationMatrix obs(6,6);
+    MatrixXd mat = obs.getMatrix();
+    cout << "matRows= " << mat.rows() << "matCols= " << mat.cols() << endl;
+    cout << "add info:" << mat.coeff(2) << endl;
+}
 
 int main()
 {
     cout << "Starting program" << endl;
+    stateTest();
     return 0;
 }
+
+
