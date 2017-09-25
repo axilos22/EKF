002218e153738a61ae4e702bc53701ae362fa01f
axilos22 <zenos_22@hotmail.com>
2017-09-26 01:31:37 +0200

    ADD: full prediction.

--- src/robotefk.cpp
+++ src/robotefk.cpp
@@ -15,6 +15,14 @@ RobotEFK::RobotEFK(const char *name) :
 
 int RobotEFK::predict(State x, Input u)
 {
-    double deltaD = (1/2.)*(u(lEncoderRank) + u(rEncoderRank));
-    double deltaTheta = (1/robotWheelSpace)*(u(lEncoderRank) - u(rEncoderRank));
+    //odometry equations - (2,0) robot
+    //STATE PREDICT
+    double deltaD = (0.5)*(u(rEncoderRank) + u(lEncoderRank));
+    double deltaTheta = (1/robotWheelSpace)*(u(rEncoderRank) - u(lEncoderRank));
+    m_X->setElement(xRank, x(xRank) + deltaD*cos(x(thetaRank) + 0.5*deltaTheta));
+    m_X->setElement(yRank, x(yRank) + deltaD*sin(x(thetaRank) + 0.5*deltaTheta));
+    m_X->setElement(thetaRank, x(thetaRank) + deltaTheta);
+    //P Matrix PREDICT
+    m_P->getMatrix() = m_F->getMatrix() * m_P->getMatrix() * m_F->getMatrix().transpose();
+    return 0;
 }
